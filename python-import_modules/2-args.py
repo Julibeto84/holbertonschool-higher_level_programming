e49fcc7e9eb472c005a168dd89c38ddcf260ae3e
Your Name <you@example.com>
2023-06-08 11:03:32 -0500

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     else:
         print(f"{len(sys.argv) - 1} arguments:")
         for i in range(1, len((sys.argv))):
-            print(f"{i}: {sys.argv[i]}")
-            
+                print(f"{i}: {sys.argv[i]}")
+
