716d766010eea2e5e3635f63c8c131c0a63a0e6e
Your Name <you@example.com>
2023-06-16 13:42:23 -0500

    3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""Define a class square"""
+""" Square - First python classes's class """
 
 
 class Square:
-    """Defines a square"""
-
+    """ Init method to instantiate """
     def __init__(self, size=0):
-        """initiates size value"""
-        if type(size) != int:
+        if (isinstance(size, int) is False):
             raise TypeError("size must be an integer")
         elif (size < 0):
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-    """Return the area of the square"""
-    def area(self):
-        return self.__size**2
 
+    """ Area - Calcs the area of the square """
+    def area(self):
+        area = self._Square__size ** 2
+        return area
