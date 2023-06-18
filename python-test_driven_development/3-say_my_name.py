6e458cba68107b81028ab331a8ed7b942eeb3811
Your Name <you@example.com>
2023-06-17 21:17:08 -0500

    3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-say_my_name = __import__('3-say_my_name').say_my_name
+"""
+    Say my name:
+    Module wich prints a name
+"""
 
-say_my_name("John", "Smith")
-say_my_name("Walter", "White")
-say_my_name("Bob")
-try:
-    say_my_name(12, "White")
-except Exception as e:
-    print(e)
+
+def say_my_name(first_name, last_name=""):
+    """
+        say_my_name - prints My name is <first name> <last name>
+    
+        Arguments:
+            first_name (str): First name to print.
+            last_name (str): Last name to print. Can be blank
+    """
+    if isinstance(first_name, str) is False:
+        raise TypeError("first_name must be a string")
+    if isinstance(last_name, str) is False:
+        raise TypeError("last_name must be a string")
+    print("My name is {:s} {:s}".format(first_name, last_name))
