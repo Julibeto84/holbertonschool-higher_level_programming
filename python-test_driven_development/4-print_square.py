732629d9826e056b23e0f26c0a62c9e1050b6ae0
Your Name <you@example.com>
2023-06-17 21:30:24 -0500

    4-print_square.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-print_square = __import__('4-print_square').print_square
+"""
+Definning a funtion wich print entire name properly.
+"""
 
-print_square(4)
-print("")
-print_square(10)
-print("")
-print_square(0)
-print("")
-print_square(1)
-print("")
-try:
-    print_square(-1)
-except Exception as e:
-    print(e)
-print("")
+
+def print_square(size):
+    """
+    Prints a square with the # char.
+    Arguments:
+        size
+    Raises:
+        TypeError  (size): Size must be an integer.
+        TypeError  (size): Size must be an integer.
+        ValueError  (< 0): Size must be >= 0.
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size is True:
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
