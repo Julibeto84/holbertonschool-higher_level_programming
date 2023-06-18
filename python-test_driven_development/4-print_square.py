74873edabcdc8108d37df6e41bca35e706f75485
Your Name <you@example.com>
2023-06-17 21:26:49 -0500

    3-say_my_name.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-"""
-    Print square:
-    This module prints a square using the # char
-"""
+print_square = __import__('4-print_square').print_square
 
-
-def print_square(size):
-    """
-    print_square - prints a square using the # char
-
-    Arguments:
-        size (int): Square size
-    """
-    if isinstance(size, int) is False:
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    if size == 0:
-        print()
-    for row in range(size):
-        for column in range(size):
-            print("#", end="")
-        print()
+print_square(4)
+print("")
+print_square(10)
+print("")
+print_square(0)
+print("")
+print_square(1)
+print("")
+try:
+    print_square(-1)
+except Exception as e:
+    print(e)
+print("")
