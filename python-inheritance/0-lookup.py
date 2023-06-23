2402cc883f02a9c14e779825aff844b4d99301f0
Your Name <you@example.com>
2023-06-23 17:17:27 -0500

    0-lookup.py

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Module to return a list of object attributes and methods
+"""
+
+
+def lookup(obj):
+    """
+        lookup - Return a list with object's attributes and method
+        Arguments:
+            obj - Object to bild the list with
+
+        Return:
+            made list.
+    """
+    return dir(obj)
