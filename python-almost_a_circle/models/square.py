ea7c64b4c9b132465d4619a5d5e36b4a3f1a22c8
Your Name <you@example.com>
2023-06-30 19:17:18 -0500

    square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,82 +1,103 @@
 #!/usr/bin/python3
 """
-Module Square
-Inherits from class Rectangle,
-That inherits from class Base
+Module 3-Square
+"""
+
+from models.rectangle import Rectangle
+"""
+Importing Rectangle Class to be inherited
 """
 
 
-from models.rectangle import Rectangle
-
-
 class Square(Rectangle):
-    """class Square inherits from class Rectangle
-    Methods:
-        def __init__(self,size,x,y,id)
-        def __str__(self)
-        def update(self, *args, **kwargs)
-        def to_dictionary(self)
-    Getter:
-       def size(self)
-    Setter:
-        def size(self, value)
+    """
+    Class Square that inherit from Rectangle Class
     """
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialization
-        calls the supper class rectangle
-        and assigns width and height to size
+        """
+        Constructor for an instance of the class Square
+
+        Args:
+            size (for both width and height)
+            x
+            y
+            id
+        they must me inherited from Rectangle
+
+        Methods:
+            area: returns the area of the object Square
+            display: prints in stdo the square using '#'
+            update: uses *args and *kwargs
+            to_dictionary: returns a dict representation of the square
         """
         super().__init__(size, size, x, y, id)
-        self.size = size
+
+    def __str__(self):
+        """
+        overriding the __str__ method so that it returns a specific message
+        """
+        msg = "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                self.width)
+        return msg
 
     @property
     def size(self):
-        """Size getter"""
+        """
+        Getter for size
+        """
         return self.width
 
     @size.setter
-    def size(self, value):
-        """size setter"""
-        self.width = value
-        self.height = value
-
-    def __str__(self):
-        """Overrides to return
-        "[Square] (<id>) <x>/<y> - <size>"
+    def size(self, size):
         """
-        return ("[Square] ({}) {}/{} - {}".format
-                (self.id, self.x, self.y, self.size))
+        Setter for size
+
+        Args:
+            size: the size of the square
+        """
+        self.width = size
+        self.height = size
 
     def update(self, *args, **kwargs):
-        """Assign arguments to attributes"""
-        if args:
-            for i, j in enumerate(args):
-                if i == 0:
-                    self.id = j
-                elif i == 1:
-                    self.size = j
-                elif i == 2:
-                    self.x = j
-                else:
-                    self.y = j
+        """
+        method that assigns an argument (from *args or **kwargs)
+        to each attribute
 
+        Args:
+            args: list of arguments, all ints
+            kwargs: assigns a key/value argument to attributes
+        """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+                if len(args) >= 2:
+                    self.size = args[1]
+                if len(args) >= 3:
+                    self.x = args[2]
+                if len(args) >= 4:
+                    self.y = args[3]
         else:
-            """kwargs"""
-            if "id" in kwargs:
-                self.id = kwargs["id"]
-            if "size" in kwargs:
-                self.size = kwargs["size"]
-            if "x" in kwargs:
-                self.x = kwargs["x"]
-            if "y" in kwargs:
-                self.y = kwargs["y"]
+            for k, v in kwargs.items():
+                if k == "id":
+                    self.id = v
+                if k == "size":
+                    self.size = v
+                if k == "x":
+                    self.x = v
+                if k == "y":
+                    self.y = v
 
     def to_dictionary(self):
-        """returns the dictionary representation of square"""
-        dic = {}
-        dic["id"] = self.id
-        dic["size"] = self.size
-        dic["x"] = self.x
-        dic["y"] = self.y
-        return dic
+        """
+        Method that returns the dictionary representation of a square,
+        containing all its attributes in a specific order:
+        id, x, size, y
+        """
+        dict_rep = {}
+        dict_rep["id"] = self.id
+        dict_rep["x"] = self.x
+        dict_rep["size"] = self.size
+        dict_rep["y"] = self.y
+        return dict_rep
+
 
