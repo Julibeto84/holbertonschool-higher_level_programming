0c62c48cd42b0f01b914fe576625883f470e8a10
Your Name <you@example.com>
2023-10-02 12:20:24 -0500

    3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 if __name__ == "__main__":
     """Print the addition of all arguments."""
     import sys
@@ -6,3 +8,4 @@ if __name__ == "__main__":
     for i in range(len(sys.argv) - 1):
         total += int(sys.argv[i + 1])
     print("{}".format(total))
+      
