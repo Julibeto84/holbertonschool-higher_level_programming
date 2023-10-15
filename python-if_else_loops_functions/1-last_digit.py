8f07aa1e3c44d37a27f547f562c4cba44880725d
Your Name <you@example.com>
2023-10-14 19:27:22 -0500

    1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,4 +8,5 @@ elif ld == 0:
     print("Last digit of {} is {} and is 0".format(number, ld))
 else:
     ld = number % -10
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, ld))
+    print("Last digit of {} is {} and is less than 6/ and not 0"
+          .format(number, ld))
