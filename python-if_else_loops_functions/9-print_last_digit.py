d8db6a9549bed073339aadfd575870c57cad51ea
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-10-18 15:13:57 -0500

    8-uppercase.py

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+ #!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for i in str:
+        if ord(i) <= ord('a') and ord(i) <= ord('z'):
+            result += chr(ord(i) - 32)
+        else:
+            result += i
+    print(result)
