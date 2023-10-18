7a2062e240b2cb73ec362596ba104993288479f7
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-10-18 16:35:45 -0500

    9-print_last_digit.py

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print(full(number) % 10, end="")
-    return (full(number) % 10)
+    if number < 0:
+        number = number * -1
+    ld = number % 10
+    print("{:d}".format(ld), end="")
+    return ld
+
