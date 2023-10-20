69a6db460cf67563f95e53a9659ecb253449639f
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-10-20 12:52:32 -0500

    3-infinite_add.py

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    num_args = len(sys.argv) - 1 
+    for i in range(num_args):
+        print("{}".format(int(num_args)))
+        
+
