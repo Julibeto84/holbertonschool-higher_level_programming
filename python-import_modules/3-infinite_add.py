f5dc8fbe1ad1e1b1b592f3841e022b47fa492030
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-10-20 16:19:03 -0500

    3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     num_args = len(sys.argv)
     total = 0
     for i in range(1, num_args):
-        total += int(sys.argv[i])
+        total += int(sys.argv[i + 1])
         print("{}".format(total))
         
