43d174584bd2a03dae31bbd148cba39126d8858b
Your Name <you@example.com>
2023-10-21 19:14:53 -0500

    'update file 4-new_in_list.py'

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    cp_list = list.copy()
-    if idx >= 0 and idx < len(my_list):
-        return cp_list()
-    cp_list[idx] = element
-    return cp_list
+    copy = my_list.copy()
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list.copy()
+    else:
+        copy[idx] = element
+        return copy
