dad4c9bc4ed5c9a61263bf47112a4edcab0c0beb
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-10-23 16:50:16 -0500

    6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for celda in (row):
-            print("{:d}".format(celda), end=" ")
-        print("")
+        for index, celda in enumerate(row):
+            if index != len(row) - 1:
+                print("{}".format(celda), end= " ")
+            else:
+                print("{}".format(celda))
+    print()
+
 
 
 if __name__ == "__main__":
--- python-data_structures/README.md
+++ python-data_structures/README.md
@@ -1,1 +1,7 @@
 Python - Data Structures: Lists, Tuples
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for celda in (row):
+            print("{:d}".format(celda), end=" ")
+        print("")
