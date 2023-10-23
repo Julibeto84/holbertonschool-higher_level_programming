0ae1c3505a7f6277b5fa9324a4e021693e60debe
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-10-23 12:19:38 -0500

    5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -5,9 +5,3 @@ def no_c(my_string):
         if i != 'c' and i != "C":
             new_str += i
     return new_str
-
-
-if __name__ == "__main__":
-    print(no_c("Best School"))
-    print(no_c("Chicago"))
-    print(no_c("C is fun!"))
