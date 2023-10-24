d8123a594d79bfad586b7faea5b49a47b1854eea
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-10-24 14:30:25 -0500

    8-multiple_returns.py

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    tuple = ()
+    if len(sentence) == 0:
+     tuple == "None"
+    else:
+       tuple = len(sentence), sentence[0]
+       return tuple
+    
+
+    if __name__ == "__main__":
+       
+        sentence = "At school, I learnt C!"
+        length, first = multiple_returns(sentence)
+        print("Length: {:d} - First character: {}".format(length, first))
