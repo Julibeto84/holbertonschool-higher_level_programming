a32a549b95924bf7179040617529e7861b88c157
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-10-24 15:06:10 -0500

    8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -7,9 +7,3 @@ def multiple_returns(sentence):
         tuple = (len(sentence), sentence[0])
     
     return tuple
-
-if __name__ == "__main__":
-
-    sentence = "At school, I learnt C!"
-    length, first = multiple_returns(sentence)
-    print("Length: {:d} - First character: {}".format(length, first))
