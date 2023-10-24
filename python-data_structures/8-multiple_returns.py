9c43d3858abda55c712abac221f8535a44528750
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-10-24 14:35:06 -0500

    8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     tuple = ()
     if len(sentence) == 0:
-     tuple = "None"
+     tuple = None
     else:
        tuple = len(sentence), sentence[0]
        return tuple
