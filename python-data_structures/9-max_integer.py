3d93d9115ff6cbaddae2052c92de6af802e4a335
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-10-24 16:26:51 -0500

    9-max_integer.py

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max = 0
+    if len(my_list) == 0:
+        return None
+    else:
+        for element in my_list:
+            if element > max:
+                max = element
+        return max
+
+        
+    
