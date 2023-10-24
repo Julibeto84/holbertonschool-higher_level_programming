ed662646b66f62220edd3adab12f2c68e864fcf1
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-10-24 13:27:55 -0500

    7-add_tuple.py

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+        tuplea = tuple_a + (0, 0)
+        tupleb = tuple_b + (0, 0)
+        new_tuple = (tuplea[0] + tupleb[0], tuplea[1] + tupleb[1])
+        return new_tuple
