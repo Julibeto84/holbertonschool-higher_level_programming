c41aeb386aefe0342e7c7e2c0e958a6c1f2413cc
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-10-24 09:59:48 -0500

    6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for index, celda in enumerate(row):
             if index != len(row) - 1:
-                print("{:d}".format(celda), end = " ")
+                print("{:d}".format(celda), end=" ")
             else:
-                print("{:d}".format(celda), end = "")
+                print("{:d}".format(celda), end="")
         print()
