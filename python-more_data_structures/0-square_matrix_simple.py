659b3435e1c4c4103fec056a18b339a19cc6c25f
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-10-26 14:23:57 -0500

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,2 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_matrix = list(range(len(matrix)))
-    for y, row in enumerate(matrix):
-        new_matrix[y] = list(range(len(matrix[y])))
-        for x, celda in enumerate(row):
-            new_matrix[y][x] = celda * celda
-    return new_matrix
+hjbh
