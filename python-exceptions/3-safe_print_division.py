94a3e8d797ffc4bbb639336cd80ee9110792e4d6
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-11-03 10:54:13 -0500

    3-safe_print_list_integers.py

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -5,6 +5,8 @@ def safe_print_division(a, b):
         result =  a / b
     except (ZeroDivisionError, TypeError):
         pass
+    else:
+        return result
     finally:
         print("Inside result: {}".format(result))
 
