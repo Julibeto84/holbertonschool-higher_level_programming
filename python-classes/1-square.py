b65b3fa3918335fed94b7bdca6a4b3532cfe4bb3
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-11-08 13:37:22 -0500

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Class will be used to create a objects that represent square."""
+    def __init__(self, size):
+        """Initialize a new square"""
+        self.__size = size
