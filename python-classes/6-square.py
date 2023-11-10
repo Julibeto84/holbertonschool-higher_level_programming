bb0428aed1f30278d49ae8f1af611b2e433de3eb
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-11-10 13:56:24 -0500

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -33,6 +33,7 @@ class Square:
                 not all(isinstance(num, int) and num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
+
     def area(self):
 
         return self.__size ** 2
