25f4fd98dedb0e89d9a637ef0747abe58ba5ef39
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-11-17 16:15:13 -0500

    creating files

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a matrix division function."""
+
+
+def matrix_divided(matrix, div):
+    """Divide all elements of a matrix."""
+    
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    
