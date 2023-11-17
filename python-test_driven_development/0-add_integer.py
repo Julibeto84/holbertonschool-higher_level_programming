84a9cce80dc5c57a4d8cb1d8f6d235ade87a02d0
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-11-17 08:47:48 -0500

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -12,11 +12,10 @@ def add_integer(a, b=98):
     If the arguments are not integers,
       a TypeError exception is thrown
     """
-    if ((not isinstance(a, int) and not
-        isinstance(a, float))):
+    not_of = lambda x, i: not isinstance(x, i)
+    if ((not_of(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not
-    isinstance(b, float))):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
 
     a = int(a)
