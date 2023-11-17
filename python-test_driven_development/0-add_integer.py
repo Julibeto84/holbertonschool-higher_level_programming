92b60af5ce0c573f85ec8b9ff3c42e60752fe4ed
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-11-17 08:17:27 -0500

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,18 +5,18 @@
 def add_integer(a, b=98):
 
     """Return the integer adds of a and b
-    
-    Floats arguments are typecasted to  
+
+    Floats arguments are typecasted to 
     ints  before addition  is performed
 
     If the arguments are not integers,
       a TypeError exception is thrown
     """
     if ((not isinstance(a, int) and not
-    isinstance(a, float))):
+        isinstance(a, float))):
         raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not
-    isinstance(b, float))):
+        isinstance(b, float))):
         raise TypeError("b must be an integer")
     
     a = int(a)
