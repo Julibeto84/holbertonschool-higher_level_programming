37f015fde6d3f596271f58fee972bac14be30a96
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-11-17 08:37:04 -0500

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -13,7 +13,7 @@ def add_integer(a, b=98):
       a TypeError exception is thrown
     """
     if ((not isinstance(a, int) and not
-    isinstance(a, float))):
+        isinstance(a, float))):
         raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not
     isinstance(b, float))):
