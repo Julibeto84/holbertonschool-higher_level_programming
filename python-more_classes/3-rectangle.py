e858bc06a7e20940bb2ac57b6cda5992fbb0a9e3
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-11-22 09:32:08 -0500

    3-rectangle.py

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -45,13 +45,13 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
-    
+
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
         rect = []
         for i in range(self.__height):
-            [rect.append ('#') for j in range(self.__width)]
+            [rect.append('#') for j in range(self.__width)]
             if i != self.__height -1:
-                rect.append ("\n")
+                rect.append("\n")
         return "".join(rect)  
